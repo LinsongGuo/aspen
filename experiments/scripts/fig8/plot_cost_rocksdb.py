4e7d55605375727423bbfa4b3a533ab39459ae95
Linsong Guo <l1guo@aspen.sysnet.ucsd.edu>
2025-03-05 12:29:51 -0800

    move fig8 scripts

--- experiments/scripts/fig8/plot_cost_rocksdb.py
+++ experiments/scripts/fig8/plot_cost_rocksdb.py
@@ -5,13 +5,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-if len(sys.argv) > 1:
-    quantum = int(sys.argv[1])
-else:
-    print("No quantum provided.")
-
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-DF_RESULT_DIR = os.path.join(SCRIPT_DIR, f'results/cost_df-{quantum}us')
+DF_RESULT_DIR = os.path.join(SCRIPT_DIR, 'results/cost_df')
 ROCKSDB_RESULT_DIR = os.path.join(SCRIPT_DIR, 'results/cost_rocksdb')
 
 TRIAL = 99
@@ -40,23 +35,24 @@ def cal_base_exe(result_dir, w, mech):
 	else:
 		return get_data(result_dir, 100000000, w, 'uintr', all=0)[0]
 
-work = ['rmv', 'ppo', 'kmeans', 'rmv+ppo+kmeans', 'ad+decay+rmv+ppo+kmeans'] 
-work_name = ['rmv', 
-			'ppo', 
-			'kmeans', 
-			r'$\text{rmv} + \text{ppo}$' '\n' r'$\text{kmeans}$',  
-    		r'$\text{ad} + \text{decay} + \text{rmv}$' '\n' r'$\text{ppo} + \text{kmeans}$']
+work = ['scan', 'scan+scan+scan+scan+scan+scan+scan+scan', 
+		'get+scan+get+scan+get+scan+get+scan+get+scan+get+scan+get+scan+get+scan+get+scan+get+scan']
+work_name = ['scan', 
+			r'$\text{scan} \times 8$', 
+			r'$\text{get} \times 8+$' '\n' r'$\text{scan} \times 8$']
+			# r'$\text{Get} \times 8$' r'$+\text{Scan} \times 8$']
 
 mech = ['signal', 'uintr', 'concord']
 mech_name = ['Signals', 'UINTR', 'Compiler']
 
+quantum = 5
 data = {}
 for m in mech:
 	m_ = m + '_all'
 	data[m_] = []
 	for idx in range(len(work)):
 		w = work[idx]
-		result_dir = DF_RESULT_DIR
+		result_dir = ROCKSDB_RESULT_DIR
 		base = cal_base_exe(result_dir, w, m)
 		exe, preempt = get_data(result_dir, quantum, w, m, all=1)
 		cost = (exe - base) / preempt * (10**6)
@@ -66,10 +62,9 @@ for m in mech:
 	data[m] = []
 	for idx in range(len(work)):
 		w = work[idx]
-		result_dir = DF_RESULT_DIR
+		result_dir = ROCKSDB_RESULT_DIR
 		base = cal_base_exe(result_dir, w, m)
-		q = 5 if m == 'uintr' else quantum
-		exe, preempt = get_data(result_dir, q, w, m, all=0)
+		exe, preempt = get_data(result_dir, quantum, w, m, all=0)
 		cost = (exe - base) / preempt * (10**6)
 		data[m].append(cost)
 		# print('Preempt:', m, w, quantum, cost)
@@ -80,48 +75,40 @@ colors_all = ['#D1EDFF', '#F4B6B6', '#FFD4A8']
 colors = ['#56B4E9', '#E14A3F', '#FF8C29']
 
 
-# x = np.arange(len(work_name))
-# x = np.array([0, 1.2, 2.5, 3.7, 4.9, 6.1, 7.3, 9])
-# x = np.array([1, 2.2, 3.4, 4.6, 5.8])
-x = np.array([1, 2.5, 4, 5.5, 7])
+
+x = np.array([1, 2.2, 3.4])
 
 
-fig, ax = plt.subplots(figsize=(8, 2.7))
+fig, ax = plt.subplots(figsize=(4.0, 2.3))
 bar_width = 0.3 
 offset = -0.3
 
 for i in range(len(mech)):
 	m = mech[i]
 	m_ = m + '_all'
-	ax.bar(x + offset + i * bar_width, data[m_], width=bar_width, color=colors_all[i], edgecolor='black', label='{} (Context-Switch Cost)'.format(mech_name[i]))
-	# print(f'{m} all: {data[m_]}')
+	ax.bar(x + offset + i * bar_width, data[m_], width=bar_width, color=colors_all[i], edgecolor='black', label='{} (context switch)'.format(mech_name[i]))
 	for i in range(len(data[m_])):
 		print(f'{m}: {data[m_][i] - data[m][i]} = {data[m_][i]} - {data[m][i]}')
 
 for i in range(len(mech)):
 	m = mech[i]
-	ax.bar(x + offset + i * bar_width, data[m], width=bar_width, color=colors[i], edgecolor='black', label='{} (Preemption Cost)'.format(mech_name[i]))
-	# print(f'{m} preempt: {data[m]}')
-
-handles, labels = ax.get_legend_handles_labels()
-new_handles = [handles[0], handles[3], handles[1], handles[4], handles[2], handles[5]]
-new_labels = [labels[0], labels[3], labels[1], labels[4], labels[2], labels[5]] 
+	ax.bar(x + offset + i * bar_width, data[m], width=bar_width, color=colors[i], edgecolor='black', label='{} (preemption)'.format(mech_name[i]))
     
 ax.set_xticks(x)
-ax.set_xticklabels(work_name, fontsize=11)
-ax.set_ylabel(r'Average Cost ($\mu{}$s)', fontsize=12)
+ax.set_xticklabels(work_name, fontsize=12)
+ax.set_ylabel(r'Average Cost ($\mu{}$s)', fontsize=14)
+ax.tick_params(axis='y', labelsize=12)
 ax.set_ylim(top=5)
-ax.set_xlim(left=0.3, right=7.7)
+ax.set_xlim(left=0.45, right=3.95)
 
-plt.legend(new_handles, new_labels, ncol=3, fontsize=11, handlelength=1.2, handletextpad=0.3, columnspacing=0.8, loc='upper center', frameon=False, bbox_to_anchor=(0.475, 1.36))
 plt.subplots_adjust(
-    left=0.057,   
+    left=0.115,   
     right=0.98, 
-    bottom=0.17,  
-    top=0.81,    
+    bottom=0.19,  
+    top=0.95,    
 )
 
 # plt.title('DataFrames (5 us)', fontsize=30)
 # plt.tight_layout()
-plt.savefig(f'figures/df_cost-{quantum}us.pdf')
+plt.savefig('figures/rocksdb_cost.pdf')
 plt.show()
