c736e7afcccd89e42b43d4102459e8f1a6544146
Linsong Guo <l1guo@aspen.sysnet.ucsd.edu>
2025-03-04 23:35:40 -0800

    remove dummy apps

--- apps/uintr_bench/goodluck.sh
+++
@@ -1,7 +0,0 @@
-# benches=(mcf base64 matmul)
-# benches=(cache_unfriendly cache_friendly cache_stripe)
-benches=(cache_unfriendly)
-
-for ((i=0; i<${#benches[@]}; i++)); do
-    ./run_cache_pollution.sh ${benches[i]}
-done
